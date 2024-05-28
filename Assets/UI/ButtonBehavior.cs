b7799cf9b1d9bd0cf8e3fc51b05eb8acfb989a51
Balsero <78754605+Balsero@users.noreply.github.com>
2024-05-28 01:34:21 -0400

    Buttons Menu Looks (done)

---
+++ Assets/UI/ButtonBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UI/ModifieText.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+
+public class ModifieText : MonoBehaviour, IPointerDownHandler,IPointerUpHandler
+{
+    [SerializeField] private TextMeshProUGUI textx;
+    [SerializeField] private float yoffset = 5.0f;
+    [SerializeField] private float alpha = 0.2f;
+    private Vector2 originalPosition;
+    private string pressedColorHex = "#F1EEE9";
+    private Color pressedColor;
+    private Color originalColor;
+    
+
+    // Start is called before the first frame updatse
+    private void Start()
+    {   
+        if(textx != null)
+        {
+            RectTransform rectTransform = textx.GetComponent<RectTransform>();
+            originalPosition = rectTransform.anchoredPosition;
+            originalColor = textx.color;
+            if(ColorUtility.TryParseHtmlString(pressedColorHex, out pressedColor))
+            {
+                pressedColor.a = alpha;
+            }
+            else
+            {
+                Debug.Log("Color not parsed");
+            }
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        RectTransform rectTransform = textx.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(originalPosition.x, originalPosition.y - yoffset);
+        textx.color = pressedColor;
+        
+        
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        RectTransform rectTransform = textx.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = originalPosition;
+        Color colorFullAlpha = originalColor;
+        colorFullAlpha.a = 1.0f;
+        textx.color = colorFullAlpha;
+        
+    }
+}
+
