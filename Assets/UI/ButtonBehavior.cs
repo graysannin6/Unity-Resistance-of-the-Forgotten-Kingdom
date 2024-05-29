31f7d9c378f332e4867780a5b6f7d92b353547bb
Balsero <78754605+Balsero@users.noreply.github.com>
2024-05-28 21:37:39 -0400

    start and quit working as expected :dog:

--- Assets/UI/ButtonBehavior.cs
+++ Assets/UI/ButtonBehavior.cs
@@ -8,11 +8,34 @@ using UnityEngine.SceneManagement;
 public class ButtonBehavior : MonoBehaviour
 {   
     public string _newGameLevel;
+    public GameObject _panelStart;
+    public GameObject _panelQuit;
+    
 
     public void NewGameDialogYes()
     {
         SceneManager.LoadScene(_newGameLevel);
     }
+
+    public void PanelOnStart()
+    {
+        _panelStart.SetActive(true);   
+    }
+
+    public void PanelOffStart()
+    {
+        _panelStart.SetActive(false);   
+    }
+
+    public void PanelOnQuit()
+    {
+        _panelQuit.SetActive(true);   
+    }
+
+    public void PanelOffQuit()
+    {
+        _panelQuit.SetActive(false);   
+    }		
 
     public void ExitButton()
     {
