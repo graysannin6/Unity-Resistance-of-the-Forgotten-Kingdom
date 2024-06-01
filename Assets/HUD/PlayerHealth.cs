b898a3d39ac19c3cbe4332f293636b5526c4910e
Balsero <78754605+Balsero@users.noreply.github.com>
2024-06-01 00:02:23 -0400

    Mechanics of the health bar , working.

--- Assets/HUD/PlayerHealth.cs
+++ Assets/HUD/PlayerHealth.cs
@@ -7,18 +7,20 @@ using TMPro;
 public class PlayerHealth : MonoBehaviour
 {   
     
-    private float heath;
+    private float health;
     private float lerpTimer;
     public float maxHealth = 8;
     public float chipSpeed = 2f;
     public Image frontHealthBar;
     public Image backHealthBar;
+    private string barRestoreColorHex = "#C9FF9E";
+    private Color restoredColor;
 
    // Start is called before the first frame update
     
     void Start()
     {
-        heath = maxHealth;
+        health = maxHealth;
         frontHealthBar.fillAmount = 1;
         backHealthBar.fillAmount = 1;
     }
@@ -27,10 +29,15 @@ public class PlayerHealth : MonoBehaviour
     void Update()
     {
         health = Mathf.Clamp(health, 0, maxHealth);
-
-        if(Input.GetKeyDown(KeyCode.Space))
+        UpdateHealthUI();
+        if(Input.GetKeyDown(KeyCode.A))
         {
             TakeDamage(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            RestoreHealth(1);
         }
 
     }
@@ -38,12 +45,48 @@ public class PlayerHealth : MonoBehaviour
     public void UpdateHealthUI()
     {
         Debug.Log(health);
+        float fillFront = frontHealthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if(fillBack > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.black;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, hFraction, percentComplete);
+        }
+        if(fillFront<hFraction)
+        {
+            backHealthBar.fillAmount = hFraction;
+            
+            lerpTimer += Time.deltaTime;
+
+            if(ColorUtility.TryParseHtmlString(barRestoreColorHex, out restoredColor))
+            {
+                backHealthBar.color = restoredColor;
+            
+            }
+            else
+            {
+                Debug.Log("Color not parsed");
+            }
+
+            float percentComplete = lerpTimer / chipSpeed;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, hFraction, percentComplete);
+        }
     }
 
 
     public void TakeDamage(float damage)
     {
         health -= damage;
+        lerpTimer = 0f;
+    }
+
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
         lerpTimer = 0f;
     }
 
