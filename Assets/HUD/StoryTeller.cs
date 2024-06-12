1943e0c7e2f9a7e4f599ef8868087f7b4a3bac14
Balsero <78754605+Balsero@users.noreply.github.com>
2024-06-12 00:29:36 -0400

    New Story :100:

--- Assets/HUD/StoryTeller.cs
+++ Assets/HUD/StoryTeller.cs
@@ -12,11 +12,17 @@ public class StoryTaller : MonoBehaviour
     {
         if (textWriter != null && messageText != null)
         {
-            string longText = "In the kingdoms forgotten by time," +
-                              " a final battle for survival is waged." +
-                              " Endless waves of the vilest creatures emerge from the heart of the earth to devour and destroy all in their path." +
-                              " After a tragic battle, a young king is the sole bulwark between his kingdom and annihilation." +
-                              " Shall he find grace in the heavens, or will this be his last crusade?" + "\n" + "Press F to continue.";
+            string longText = "In an age of iron and fire, the last sovereign, King Aric, "+
+                               "stands against an endless tide of monstrous foes. Clad in crimson raiments and an azure cloak," +
+                               "he wields a blade blessed by the gods, its edge sharp enough to cleave the essence of evil. " +
+                               "The ground beneath his feet is littered with the bones of fallen warriors, "+
+                               "the air thick with the stench of decay. " +
+                               "From the shadows emerge the relentless legions of goblins, skeletons, and bats, wrought from the darkest corners of the abyss. " +
+                            "Will the young king overcome the hordes of darkness, or is this the end of humanity? As the last one standing, " +
+                            "King Aric's fate is intertwined with the survival of his kingdom. His struggle is not just for his life, " +
+                            "but for the very soul of a world teetering on the brink of annihilation. Stand with him, witness his valor, "+ 
+                            "and wonder—can he hold back the night, or will the shadows finally prevail?"+ "\n" +
+                            "Press F to continue ...";
             textWriter.AddWriter(messageText, longText, 0.07f);
         }
         else
