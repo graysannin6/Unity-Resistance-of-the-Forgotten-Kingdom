bbe1699357ccfbfea71128d92c56d13b0675399c
Balsero <78754605+Balsero@users.noreply.github.com>
2024-07-08 00:34:05 -0400

    changes

--- Assets/HUD/Timer.cs
+++ Assets/HUD/Timer.cs
@@ -1,16 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float elapsedTime;
-    void Start()
+    private float elapsedTime;
+    private static Timer instance;
+
+    void Awake()
     {
-        
+        // Ensure only one instance of the timer exists
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -20,6 +29,5 @@ public class Timer : MonoBehaviour
         int minutes = Mathf.FloorToInt(elapsedTime / 60);
         int seconds = Mathf.FloorToInt(elapsedTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        
     }
 }
